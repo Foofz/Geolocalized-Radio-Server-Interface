4356d06630afd61bbd3c026892ef48bb80545da3
Fouad Tabsh <takethiskitteh@gmail.com>
2014-07-30 13:48:29 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Server Interface </title>
+	<link rel="stylesheet" type="text/css" href="map-style.css"/>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUCdihoRA6IcSVA9a1OA_OrzXa-sAETOk&libraries=visualization,geometry&sensor=false"></script>
+	<script src="map.js"></script>
+</head>
+
+<body>
+	<div id="map-canvas"></div>
+	<div id="controls">
+		<input type="radio">
+	</div>
+</body>
+</html>
---
+++ map.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	startup();
+});
+
+function startup(){
+	var mapOptions = {
+		center: new google.maps.LatLng(42.726883, -84.48964),
+		zoom: 19,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
+	};
+
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+}
+
+function placeMarker(location) {
+  var marker = new google.maps.Marker({
+      position: location,
+      map: map
+  });
+}
