48fd4b00c5c952d8394098c03b6d93e467f9d0ac
Fouad Tabsh <takethiskitteh@gmail.com>
2014-07-31 14:30:02 -0700

    Request existing hotspots from database upon loading and display them

--- map.js
+++ map.js
@@ -1,23 +1,42 @@
-var add = [];
-var remove = [];
-var modify = [];
-
 $(document).ready(function(){
 	startup();
 });
 
 function startup(){
+	//Map options:
 	var mapOptions = {
 		center: new google.maps.LatLng(42.726883, -84.48964),
 		zoom: 19,
 		mapTypeId: google.maps.MapTypeId.SATELLITE
 	};
 
+	//Initialize map:
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+	//Add listener to single clicks on map:
 	google.maps.event.addListener(map, 'click', function(event) {
     placeMarker(event.latLng);
   });
+
+	//Use AJAX to request existing markers from database:
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {
+  		if (ajax.readyState == 4) {   // 4 means request is finished
+    		//Handle JSON response:
+    		var results = JSON.parse(this.responseText);
+    		for (var i=0; i<results.hotspots.length; i++){
+    			var marker = new google.maps.Marker({
+    				position: new google.maps.LatLng(results.hotspots[i].lat, results.hotspots[i].long),
+    				color: blue,
+      				map: map
+  				});
+    		}
+  		}
+	};
+
+	ajax.open("get", "../kdtree/requestHotSpots.php", true);
+	ajax.send(null);
+
 }
 
 //Add a new hotSpot:
@@ -46,8 +65,6 @@ function placeMarker(location) {
   	lat: location.lat(),
   	long: location.lng()
   };
-
-  add.push(hotSpot);
 
   /*alert(hotSpot.type);
   alert(hotSpot.lat);
